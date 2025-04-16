8ad24e39cb89f3db0976538ae81e0b608143f6c3
Laziz <104445440+Emotion321@users.noreply.github.com>
2025-04-16 14:22:55 +0500

    some fascinating changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
   let name = "Svelte";
+  import Nested from "./Nested.svelte";
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
+
+<style>
+  h1 {
+    color:aqua;
+  }
+</style>
+<Nested />
---
+++ src/routes/Nested.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let name = "Some gigantic, fascinating component";
+</script>
+
+<h1>Hey, I am {name}</h1>
+
+<style>
+  h1 {
+    font-size: 10em;
+    color: blueviolet;
+  }
+</style>
