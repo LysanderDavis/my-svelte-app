395b8453d6ef3bf2f07a914ca2f86288b4b56fcb
Laziz <104445440+Emotion321@users.noreply.github.com>
2025-04-18 15:10:31 +0500

    counter with total solver

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
   import Nested from "./Nested.svelte";
-  let numbers = $state([1,2,3,4]);
+
+  let numbers = $state([1, 2, 3, 4]);
+  let total = $derived(numbers.reduce((t, n) => t + n, 0));
 
   function addNumber() {
     numbers.push(numbers.length + 1);
   }
 </script>
 
-<p>{numbers.join(' + ')} = ...</p>
+<p>{numbers.join(' + ')} = {total}</p>
 
 <button onclick={addNumber}>
   Add a number
